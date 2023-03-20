96e5bcf404bbc6f157dd5e2f4ec57072ed19c912
Cooper <congqin.chen@ryerson.ca>
2023-03-20 17:57:50 -0400

    Updates

---
+++ precache-manifest.394499888c447411d9c19fab026e7fb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b818a0d73fc74a725796",
+    "url": "/react-practice/static/css/main.3d9c9828.chunk.css"
+  },
+  {
+    "revision": "b818a0d73fc74a725796",
+    "url": "/react-practice/static/js/main.b818a0d7.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/react-practice/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/react-practice/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "faaf2ed3273f0cf5b606",
+    "url": "/react-practice/static/js/runtime~main.faaf2ed3.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-practice/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b484f7377ca346cf7b7b70dddb3a6b5d",
+    "url": "/react-practice/index.html"
+  }
+];
