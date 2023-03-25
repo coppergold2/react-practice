ee4f96fe51de50a3cd2523bf8ad16ca31d47bff9
coppergold2 <c1336873249@gmail.com>
2023-03-25 17:32:53 -0400

    progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TodoList from './TodoList'
 
 
 function App() {
+  const [todos,setTodos] = useState(['Todo1', 'Todo 2'])
   return (
-    <TodoList />
+    <>
+    <TodoList todos = {todos}/>
+    <input type = "type" />
+    <button>Add Todo</button>
+    <button>CLear Complete</button>
+    <div>0 letft to do</div>
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement).render(<App />);
 
